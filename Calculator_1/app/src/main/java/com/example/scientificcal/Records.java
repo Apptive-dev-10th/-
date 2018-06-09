d20bc454893b7f6f240b512385a9ea99dfb8746c
jm1224k <jm1224k@gmail.com>
2018-06-09 12:56:14 +0900

    버튼 수정

---
+++ Calculator_1/app/src/main/java/com/example/scientificcal/Records.java
@@ -0,0 +1,13 @@
+package com.example.scientificcal;
+
+public class Records {
+    private String equation;
+
+    public String getEquation() {
+        return equation;
+    }
+
+    public void setEquation(String equation) {
+        this.equation = equation;
+    }
+}
